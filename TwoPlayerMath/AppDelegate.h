4dc4767cbb33eff18598438813383735dd3d68a6
narendrathapa <thapa.narendra@gmail.com>
2016-01-18 15:09:51 -0500

    Initial Commit

---
+++ TwoPlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayerMath
+//
+//  Created by Narendra Thapa on 2016-01-18.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
